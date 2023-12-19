6be76b69ee9e55ef622999a01427020dfb27be37
Vedant Singh <vedantc252@gmail.com>
2023-12-20 02:24:18 +0530

    Adding Functionality

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , jsonify , request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
+    return render_template("index.html")
+
+@app.route('/api' , methods =["GET", "POST"])
+def qa():
+    if request.method == "POST":
+        data = {"result" :"Thank you!"}
+        return jsonify(data)
     return render_template("index.html")
 app.run(debug = True)
