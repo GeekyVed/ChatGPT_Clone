bcd5615fe0d0df46146941aa58a42eb09831032b
Vedant Singh <vedantc252@gmail.com>
2023-12-19 18:56:09 +0530

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  theme: {
+    extend: {colors:{
+      chatblack : '#343541',
+    }},
+    
+  },
+  plugins: [],
+}
+
